b770f4ed3f7dc1dfcc88c913ae63a38117a1ecfc
sakunchamikara <2017cs014@stu.ucsc.cmb.ac.lk>
2020-07-09 19:43:15 +0530

    error commented in termsConroller

--- Backend/Backend/src/main/java/com/WasteManagementSystem/Backend/controller/TermsController.java
+++ Backend/Backend/src/main/java/com/WasteManagementSystem/Backend/controller/TermsController.java
@@ -28,65 +28,60 @@ import com.WasteManagementSystem.Backend.entity.Terms;
 import com.WasteManagementSystem.Backend.repository.TermsRepository;
 //import com.WasteManagementSystem.Backend.service.TermsService;
 
-
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TermsController {
-	
-	@Autowired
 
-	
-	private TermsRepository termsrepo;
-	
-	
-	@PostMapping("/terms")
+    @Autowired
+
+    private TermsRepository termsrepo;
+
+    @PostMapping("/terms")
     public Terms createTerms(@Valid @RequestBody Terms terms, BindingResult bindingResult) {
-		if (bindingResult.hasErrors()) {
-			return null;
-		}
+        if (bindingResult.hasErrors()) {
+            return null;
+        }
         return termsrepo.save(terms);
     }
-	@GetMapping("/terms")
+
+    @GetMapping("/terms")
     public List<Terms> getAllTerms() {
         return termsrepo.findAll();
     }
-	
-	@DeleteMapping("/terms/{id}")
-    public Map<String, Boolean> deleteTerms(@PathVariable(value = "id") int termsId)
-         throws ResourceNotFoundException {
+
+    @DeleteMapping("/terms/{id}")
+    public Map<String, Boolean> deleteTerms(@PathVariable(value = "id") int termsId) throws ResourceNotFoundException {
         Terms terms = termsrepo.findById(termsId)
-       .orElseThrow(() -> new ResourceNotFoundException("Terms not found for this id :: " + termsId));
+                .orElseThrow(() -> new ResourceNotFoundException("Terms not found for this id :: " + termsId));
 
         termsrepo.delete(terms);
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
         return response;
     }
-	
-	@PutMapping("/terms/{id}")
+
+    @PutMapping("/terms/{id}")
     public ResponseEntity<Terms> updateTerms(@PathVariable(value = "id") int termsId,
-         @Valid @RequestBody Terms termsDetails) throws ResourceNotFoundException {
+            @Valid @RequestBody Terms termsDetails) throws ResourceNotFoundException {
         Terms terms = termsrepo.findById(termsId)
-        .orElseThrow(() -> new ResourceNotFoundException("Terms not found for this id :: " + termsId));
+                .orElseThrow(() -> new ResourceNotFoundException("Terms not found for this id :: " + termsId));
 
         terms.setTitle(termsDetails.getTitle());
         terms.setTurl(termsDetails.getTurl());
-        console.log("urlll"+termsDetails.getTurl());
+        // console.log("urlll" + termsDetails.getTurl());
         terms.setTdiscrip(termsDetails.getTdiscrip());
-       console.log("discripp")+termsDetails.getTdiscrip();
+        // console.log("discripp")+termsDetails.getTdiscrip();
 
         final Terms updatedTerms = termsrepo.save(terms);
         return ResponseEntity.ok(updatedTerms);
     }
-	
-	@GetMapping("/terms/{id}")
+
+    @GetMapping("/terms/{id}")
     public ResponseEntity<Terms> getTermsById(@PathVariable(value = "id") int termsId)
-        throws ResourceNotFoundException {
+            throws ResourceNotFoundException {
         Terms terms = termsrepo.findById(termsId)
-          .orElseThrow(() -> new ResourceNotFoundException("Terms not found for this id :: " + termsId));
+                .orElseThrow(() -> new ResourceNotFoundException("Terms not found for this id :: " + termsId));
         return ResponseEntity.ok().body(terms);
     }
-	
-	
-	
+
 }

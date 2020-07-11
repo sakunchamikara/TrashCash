4a5e2d91b62b1c6735d5b156fc8905b5fb5a50e4
sakunchamikara <2017cs014@stu.ucsc.cmb.ac.lk>
2020-07-11 20:52:43 +0530

    remove console.log in terms and productcat components

--- frontend/src/app/modules/product-cat/insert-product-cat/insert-product-cat.component.ts
+++ frontend/src/app/modules/product-cat/insert-product-cat/insert-product-cat.component.ts
@@ -5,56 +5,49 @@ import { Category } from 'src/app/pojo/category';
 import { Router } from '@angular/router';
 // import { debuglog } from 'util';
 
-
 @Component({
   selector: 'app-insert-product-cat',
   templateUrl: './insert-product-cat.component.html',
-  styleUrls: ['./insert-product-cat.component.scss']
+  styleUrls: ['./insert-product-cat.component.scss'],
 })
 export class InsertProductCatComponent implements OnInit {
-
   productCat = new Category();
   submitted = false;
 
-  
-
-  constructor(private productcatService: ProductcatService,
-    private router: Router) { }
+  constructor(
+    private productcatService: ProductcatService,
+    private router: Router
+  ) {}
 
   successMsg: any;
   errorMsg: any;
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  newProductCat(): void {
-    this.submitted = false;
-   this.productCat = new Category();
-  }
-  
+  // newProductCat(): void {
+  //   this.submitted = false;
+  //   this.productCat = new Category();
+  // }
+
   save() {
-   
-    this.productcatService.createProductCat(this.productCat)
-    .subscribe((data)=> {
-    console.log(data)
-    this.productCat = new Category();
-    this.successMsg = `Product Category added successfully !`;
-   //this.gotoList();
-   // console.log('#got');
-  },
-  (error)=>{
-    this.router.navigate(['system']);
-  });
-}
+    this.productcatService.createProductCat(this.productCat).subscribe(
+      (data) => {
+        this.productCat = new Category();
+        this.successMsg = `Product Category added successfully !`;
+      },
+      (error) => {
+        console.log(error);
+        this.errorMsg = 'Something went wrong';
+      }
+    );
+  }
 
   onSubmit() {
-    
     this.submitted = true;
-    this.save();    
+    this.save();
   }
 
   gotoList() {
     this.router.navigate(['system/viewProductCat']);
   }
-
 }
--- frontend/src/app/service/productcat.service.ts
+++ frontend/src/app/service/productcat.service.ts
@@ -1,37 +1,33 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';  
-import { Observable } from 'rxjs';  
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 //import { ItemCat } from 'src/app/pojo/item-cat';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductcatService {
+  private baseUrl = "http://localhost:8080/productCats";
 
-  private baseUrl = 'http://localhost:8080/productCats';
-
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getProductCatList(): Observable<any> {
     return this.http.get(`${this.baseUrl}`);
   }
 
-  createProductCat(productcat: object): Observable<object> {  
-    return this.http.post<any>(`${this.baseUrl}` , productcat);  
-  }  
+  createProductCat(productcat: object): Observable<object> {
+    return this.http.post<any>(`${this.baseUrl}`, productcat);
+  }
 
   deleteProductCat(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: "text" });
   }
 
   getProductCat(id: number): Observable<any> {
     return this.http.get(`${this.baseUrl}/${id}`);
   }
 
-  updateProductCat(id:number,value:any):Observable<Object>{
-    return this.http.put(`${this.baseUrl}/${id}`,value);
+  updateProductCat(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
   }
- 
-
 }

3099012860d25999ba5726d4630fabf69f47703b
sakun chamikara <ssakunchamikara@gmail.com>
2020-10-04 20:52:35 +0530

    bug fixed

--- Backend/Backend/src/main/java/com/WasteManagementSystem/Backend/customer/orders/CartOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.WasteManagementSystem.Backend.customer.orders;
-
-import javax.persistence.criteria.Order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Order, Integer>{
-
-}

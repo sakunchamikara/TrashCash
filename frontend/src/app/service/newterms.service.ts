303b4cec306d8b1ac57d367de3050f3f1821ac32
sakun chamikara <ssakunchamikara@gmail.com>
2020-10-06 16:05:14 +0530

    fix some bugs in terms

--- frontend/src/app/service/newterms.service.ts
+++ frontend/src/app/service/newterms.service.ts
@@ -1,30 +1,32 @@
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ConfirmationDialogComponent } from '../modules/confirmation-dialog/confirmation-dialog.component';
+import { Terms } from '../pojo/terms';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class NewtermsService {
-  getRandomTerms(): Observable<any>{
-    const url = 'httpp://loacalhost:8080/getRandomTerms';
-    return this.http.get(url);
-    
-  }
-  private baseUrl = "http://localhost:8080/terms";
+  private  baseUrl = 'http://localhost:8080/terms';
+
   constructor(private http: HttpClient, private modalService: NgbModal) {}
 
-  createNewterm(terms: Object): Observable<Object> {
+  getRandomTerms(): Observable<any> {
+    const url = 'httpp://loacalhost:8080/getRandomTerms';
+    return this.http.get(url);
+  }
+
+  createNewterm(terms: Terms): Observable<Terms> {
     return this.http.post<any>(`${this.baseUrl}`, terms);
   }
-  getNewtermList(): Observable<any> {
-    return this.http.get(`${this.baseUrl}`);
+  getNewtermList(): Observable<Terms[]> {
+    return this.http.get<any>(`${this.baseUrl}`);
   }
 
   deleteTerm(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: "text" });
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
   }
 
   getTerm(id: number): Observable<any> {
@@ -35,8 +37,8 @@ export class NewtermsService {
     return this.http.get(`${this.baseUrl}/${id}`);
   }
 
-  updateTerm(id: number, value: any): Observable<Object> {
-    return this.http.put(`${this.baseUrl}/${id}`, value);
+  updateTerm(id: number, value: any): Observable<Terms> {
+    return this.http.put<Terms>(`${this.baseUrl}/${id}`, value);
   }
 
   public confirm(
@@ -44,16 +46,16 @@ export class NewtermsService {
     message: string,
     btnOkText: string = 'OK',
     btnCancelText: string = 'Cancel',
-    dialogSize: 'sm'|'lg' = 'sm'): Promise<boolean> {
-    const modalRef = this.modalService.open(ConfirmationDialogComponent, { size: dialogSize });
+    dialogSize: 'sm' | 'lg' = 'sm'
+  ): Promise<boolean> {
+    const modalRef = this.modalService.open(ConfirmationDialogComponent, {
+      size: dialogSize,
+    });
     modalRef.componentInstance.title = title;
     modalRef.componentInstance.message = message;
     modalRef.componentInstance.btnOkText = btnOkText;
     modalRef.componentInstance.btnCancelText = btnCancelText;
 
     return modalRef.result;
-
-}
-
-
+  }
 }
